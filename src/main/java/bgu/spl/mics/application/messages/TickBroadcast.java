aed641250fbf50ef4b3535c17b571890be3c7580
ofekshem <ofekshem@post.bgu.ac.il>
2018-12-17 15:08:00 +0200

    First commit

--- src/main/java/bgu/spl/mics/application/messages/TickBroadcast.java
+++ src/main/java/bgu/spl/mics/application/messages/TickBroadcast.java
@@ -1,4 +1,16 @@
 package bgu.spl.mics.application.messages;
-
+//Ofek
 public class TickBroadcast {
+
+    private int currentTick;
+
+    public TickBroadcast(int tick) {this.currentTick = tick;}
+
+    public int getTick(){
+        return this.currentTick;
+    }
+
+    public void increasTick() {
+        currentTick = currentTick + 1;
+    }
 }
